d1583d010b52c5b57917aede9a38b45ccc88e374
Sanusi Yusuf Korede <sanusikorede@gmail.com>
2019-02-27 11:47:32 +0100

    adding fetching data module

--- App.js
+++ App.js
@@ -1,15 +1,60 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, SafeAreaView } from 'react-native';
+import { Location, Permissions } from 'expo';
+import Map from './src/component/MapView';
+import YelpService from './src/services/yelp';
+
+const deltas = {
+  latitudeDelta: 0.0922,
+  longitudeDelta: 0.0421 
+};
 
 export default class App extends React.Component {
+  state = {
+    region: null,
+    coffeeShops: []
+  }
+
+  componentWillMount() {
+    this.getLocationAsyc();
+  }
+
+  getCoffeeShops = async () => {
+    const { latitude, longitude } = this.state.region;
+    const userLocation = { latitude, longitude };
+    const coffeeShops =  await YelpService.getCoffeeShops(userLocation);
+    this.setState({ coffeeShops });
+  }
+  
+  getLocationAsyc = async ()=> {
+    let { status } = await Permissions.askAsync(Permissions.LOCATION);
+    if ( status !== 'granted') {
+      this.setState({
+        errorMessage: 'Permission to access location was denied'
+      });
+    }
+
+    let location = await Location.getCurrentPositionAsync({});
+    const region = {
+      latitude: location.coords.latitude,
+      longitude: location.coords.longitude,
+      ...deltas
+    };
+    await this.setState({ region });
+    await this.getCoffeeShops();
+  } 
+ 
   render() {
+    const { region, coffeeShops } = this.state;
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
+     <SafeAreaView style={styles.container}>
+      <Map 
+        region={region}
+        places={coffeeShops} />
+     </SafeAreaView>
     );
   }
-}
+} 
 
 const styles = StyleSheet.create({
   container: {
---
+++ src/services/yelp.js
@@ -0,0 +1,33 @@
+import axios from 'axios';
+
+const YELP_API_KEY = 'qLXGoQV_l6WFoDLbapbPVAwkou2O7ZixcDxsgKIqCFWHuoT0C-tUA1md1Vcg1dZYcr_e7MFXB7g_8fZsUsZBc7F4q69UIq--RsSjhM0E9XEqGeguBWY7NLoiMFt2XHYx';
+
+const api = axios.create({
+baseURL: 'https://api.yelp.com/v3',
+headers: {
+    Authorization: `Bearer ${YELP_API_KEY}`
+    },
+});
+
+const getCoffeeShops = userLocation => {
+    return api.get('businesses/search', {
+        params: {
+            limit:10,
+            categories: 'coffee,coffeeroasteries,coffeeshops',
+            ...userLocation,
+        }
+    })
+    .then(res => {
+        res.data.businesses.map(business => {
+            return {
+                name: business.name,
+                coords: business.coordinates,
+            };
+        });
+    }) 
+    .catch(error => console.error(error));
+}
+
+export default {
+    getCoffeeShops,
+};
